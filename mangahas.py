0ed25c0a14cf1069c4e36aa85fd1670b89426287
TJMangahas <mtjmangahas1@tip.edu.ph>
2023-09-29 11:51:58 +0800

    New version of mangahas.py

--- mangahas.py
+++ mangahas.py
@@ -1,3 +1,5 @@
-print("Timothy James T. Mangahas\n")
-print("Timothy James T. Mangahas\n")
-print("Timothy James T. Mangahas\n")
+firstname = input("What is your first name?")
+middlename= input("What is your middle name?")
+lastname = input("What is your last name?")
+
+print(firstname + " " + middlename + " " + lastname)
